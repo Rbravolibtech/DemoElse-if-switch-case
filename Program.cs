a20c27f73d3a3e2ac12d1472e2ec8a39ae102ea6
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-11 14:34:45 -0700

    used else, if, case, statements

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DemoElse_if_switch_case
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            var season = Season2.Autumn;
+
+            switch (season)
+            {
+                case Season2.Autumn:
+                    //Console.WriteLine("It's Autumn and a beautiful season.");
+                    //break;
+
+                case Season2.Summer:
+                    Console.WriteLine("It's too hot in the summer time.");
+                    break;
+
+                default:
+                    Console.WriteLine("Winter is the best for snowboarding.");
+                    break;
+            }
+
+            // Original code for time-based condition
+            // int hour = 10;
+            // if (hour >= 0 && hour < 12)
+            // {
+            //     Console.WriteLine("It's Morning");
+            // }
+            // else if (hour >= 12 && hour < 18)
+            // {
+            //     Console.WriteLine("It's Afternoon");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("It's Evening");
+            // }
+
+            // New code for a customer-based condition
+
+            //bool isGoldCustomer = true;
+
+            //float price = (isGoldCustomer) ? 19.95f : 29.95f;
+
+            //Console.WriteLine(price);
+        }
+    }
+}
---
+++ season2.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoElse_if_switch_case
+{
+    enum Season2
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+}
